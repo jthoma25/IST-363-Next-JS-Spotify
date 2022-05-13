5678691d58e40b37deabe839de7816ba84d9e5f2
jthoma25 <78917221+jthoma25@users.noreply.github.com>
2022-05-13 16:56:21 -0400

    sick

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     images: {
-        domains: ['ist-363-spotify.local', 'https://ist-363-next-js-spotify-flax.vercel.app/artists']
+        domains: ['ist-363-spotify.local', 'https://ist-363-next-js-spotify-flax.vercel.app/']
     }
 }
