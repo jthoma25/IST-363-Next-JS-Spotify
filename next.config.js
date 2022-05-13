4820775a880615e6d07d20ee07d4d987d260620d
jthoma25 <78917221+jthoma25@users.noreply.github.com>
2022-05-13 16:52:54 -0400

    last commit for real

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
     images: {
-        domains: ['ist-363-spotify.local', 'https://ist-363-next-js-spotify-flax.vercel.app/']
+        domains: ['ist-363-spotify.local', 'https://ist-363-next-js-spotify-flax.vercel.app/artists']
     }
 }
