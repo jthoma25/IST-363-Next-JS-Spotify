0664b422781e54f27136aef7eacf254d49d0db09
jthoma25 <78917221+jthoma25@users.noreply.github.com>
2022-05-16 10:39:29 -0400

    removed tracks id

--- pages/tracks/index.js
+++ pages/tracks/index.js
@@ -6,7 +6,7 @@ import {getTracks} from '../../lib/api'
 
 export async function getStaticProps() {
     // Get external data from the file system, API, DB, etc.
-    const tracks = getTracks();
+    const tracks = await getTracks();
   
     // The value of the `props` key will be
     //  passed to the `Home` component
